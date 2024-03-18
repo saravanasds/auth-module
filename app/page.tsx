ab6f1995ac4895ff0b59e1456056c735b81e42e1
sdspalani <sdspalani@gmail.com>
2024-03-18 17:41:28 +0530

    Home and Login updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,28 @@
+import { Poppins  } from "next/font/google";
+import { cn  } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const font = Poppins ({
+  subsets: ["latin"],
+  weight: ["600"]
+})
+
 export default function Home() {
   return (
-   <p className ="font-bold text-green-500">Welcome</p>
-  );
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-green-400 to-blue-600">
+      <div className="space-y-6 text-center">
+        <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md")}>
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">
+          An Authentication for Admin Panel
+        </p>
+        <div>
+          <Button variant="secondary" size="lg">
+            Sign in
+          </Button>
+        </div>
+      </div>
+    </main>
+  )
 }
